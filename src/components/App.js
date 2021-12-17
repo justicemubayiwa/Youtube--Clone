7e82b89e33a9d3ca0b74d324089ba0943a7229c9
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-12-17 15:52:06 -0500

    Add header that refreshes to default search

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,27 @@ import useVideos from '../hooks/useVideos'
 
 const App=()=> {
     const [selectedVideo, setSelectedVideo] = useState(null)
-    const [videos, search] = useVideos('C 63 AMG');
+    const [videos, search] = useVideos('Kobe');
 
     useEffect(()=>{
         setSelectedVideo(videos[0])
     }, [videos])
+
+    function refreshPage() {
+        window.location.reload(false);
+      }
    
         return (
-            <div className="ui container">
+            <div 
+                style={{marginTop:'10px'}} 
+                className="ui container ">
+                <h1 
+                    style={{color: 'red', WebkitTextStroke:'1px black', fontSize:'40px', cursor: 'pointer'}} 
+                    class="ui center aligned header"
+                    onClick={refreshPage}
+                    >
+                        reacTube
+                </h1>
                 <SearchBar onFormSubmit={search}/>
                 <div className='ui grid'>
                     <div className='ui row'>
